dfee231d70c8aad0ec82d19ddcafd162eb4ef5c3
Antonio Aloisio <gnuton@gnuton.org>
2013-05-09 22:20:25 +0300

    Added main activity. Splash screen is shown for 2 seconds.

--- Android/Droidxperiment/src/org/gnuton/droidxperiment/SplashScreen.java
+++ Android/Droidxperiment/src/org/gnuton/droidxperiment/SplashScreen.java
@@ -2,13 +2,40 @@ package org.gnuton.droidxperiment;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.util.Log;
 
 public class SplashScreen extends Activity {
+	private static final int SPLASH_DURATION = 2; // 2 seconds
+	private static final String TAG = "SplashScreen";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
+		// Start timer and launch main activity
+		IntentLauncher launcher = new IntentLauncher();
+		launcher.start();
+	}
+
+	private class IntentLauncher extends Thread {
+		@Override
+		/**
+		 * Sleep for some time and than start new activity.
+		 */
+		public void run() {
+			try {
+				// Sleeping
+				Thread.sleep(SPLASH_DURATION * 1000);
+			} catch (Exception e) {
+				Log.e(TAG, e.getMessage());
+			}
+
+			// Start main activity
+			SplashScreen ss = SplashScreen.this;
+			Intent intent = new Intent(ss, MainActivity.class);
+			ss.startActivity(intent);
+			ss.finish();
+		}
 	}
 }
