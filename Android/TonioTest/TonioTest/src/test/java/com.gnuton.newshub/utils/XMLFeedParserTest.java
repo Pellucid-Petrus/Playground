464c1d60a76b9d5520852a09768cad9e2a9eca67
Antonio.aloisio <gnuton@gnuton.org>
2013-06-25 22:16:36 +0300

    Added stub functional tests. Still not working because of broken android studio

---
+++ Android/TonioTest/TonioTest/src/test/java/com.gnuton.newshub/utils/XMLFeedParserTest.java
@@ -0,0 +1,24 @@
+package com.gnuton.newshub.utils;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by gnuton on 6/24/13.
+ */
+public class XMLFeedParserTest extends TestCase {
+    private XMLFeedParser mParser;
+    public void setUp() throws Exception {
+        super.setUp();
+        mParser = new XMLFeedParser(null);
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testParseXML() throws Exception {
+        assertEquals(1,0);
+    }
+
+
+}
