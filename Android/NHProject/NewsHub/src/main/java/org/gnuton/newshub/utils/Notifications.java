4f9f66166df98ce6288a5dd6e8216dd1fd0c7989
Antonio.aloisio <gnuton@gnuton.org>
2013-10-05 00:09:59 +0300

    Fix crash

--- Android/NHProject/NewsHub/src/main/java/org/gnuton/newshub/utils/Notifications.java
+++ Android/NHProject/NewsHub/src/main/java/org/gnuton/newshub/utils/Notifications.java
@@ -1,7 +1,11 @@
 package org.gnuton.newshub.utils;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.widget.Toast;
+
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.MapBuilder;
 
 /**
  * Created by gnuton on 6/5/13.
@@ -20,15 +24,40 @@ public class Notifications {
         if (MyApp.mMainActivity == null)
             return;
 
-        MyApp.mMainActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Context context = MyApp.getContext();
+        if (MyApp.mMainActivity != null)
+            MyApp.mMainActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Context context = MyApp.getContext();
 
-                int duration = Toast.LENGTH_LONG;
-                Toast toast = Toast.makeText(context, msg, duration);
-                toast.show();
-            }
-        });
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context, msg, duration);
+                    toast.show();
+                }
+            });
+    }
+
+    public static void showErrorMsg(final int resourceMsg) {
+        final Context context = MyApp.mMainActivity;
+        final String msg = MyApp.getContext().getResources().getString(resourceMsg);
+        if (MyApp.mMainActivity != null)
+            MyApp.mMainActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    new AlertDialog.Builder(context).setMessage(msg).setNeutralButton(android.R.string.ok, null).show();
+                    EasyTracker easyTracker = EasyTracker.getInstance(context);
+
+                    // MapBuilder.createEvent().build() returns a Map of event fields and values
+                    // that are set and sent with the hit.
+                    easyTracker.send(MapBuilder
+                            .createEvent("error",     // Event category (required)
+                                    "error_msg",  // Event action (required)
+                                    msg,   // Event label
+                                    null)            // Event value
+                            .build()
+                    );
+
+                }
+            });
     }
 }
