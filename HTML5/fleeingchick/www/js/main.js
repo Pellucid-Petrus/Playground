b5b8d05abdf18f79635016090216caeeff86b227
Antonio Aloisio <antonio.aloisio@nokia.com>
2014-03-14 16:40:48 +0200

    This should fix a bug in the screen size

--- HTML5/fleeingchick/www/js/main.js
+++ HTML5/fleeingchick/www/js/main.js
@@ -5,25 +5,7 @@ var FLAP = 320;
 var SPAWN_RATE = 1 / 1.2;
 var OPENING = 130;
 
-/*
-WebFontConfig = {
-    google: { families: [ 'Press+Start+2P::latin' ] },
-    active: main
-};
-
-
-(function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-})();
-*/
-
-main();
+document.addEventListener("deviceready", main, false);
 
 function main() {
 
