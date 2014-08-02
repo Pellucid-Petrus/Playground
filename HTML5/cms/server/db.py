b9d36b742c1447c561796f92a459c580fcf0fc99
Antonio.aloisio <gnuton@gnuton.org>
2014-08-03 00:11:15 +0300

    Import db.py class

---
+++ HTML5/cms/server/db.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import MySQLdb as mdb
+import sys
+
+class db:
+  def __init__(self):
+    print "init"
+    self.con = None
+    self.cur = None
+
+  def connect(self, config):
+    print "connecting"
+    if (self.con):
+      print "Already connected to DB"
+      return
+    try:
+      self.con = mdb.connect(config["host"], config["user"], config["password"], config["db"])
+      self.cur = self.con.cursor()
+    except Exception as e:
+      print "Fatal error: " + str(e)
+      sys.exit(-1) 
+  
+  def disconnect(self):
+    print "disconnect"
+    self.con.close()
+    self.con = None
+    self.cur = None
+
+  def query(self, query):
+    try:
+      self.cur.execute(query)
+      return self.cur.fetchall()
+    except mdb.ProgrammingError as e:
+      print "Warning: Failed to run the query %s. The problem: %s" % (query, str(e))
+
+if __name__ == "__main__":
+  print "testing"
+  d = db()
+  config = {}
+  config["host"] = "localhost"
+  config["user"] = "testuser"
+  config["password"] = "testuser"
+  config["db"] = "testdb"
+ 
+  d.connect(config)
+  d.query("SELECT * FROM Writers")
+  d.disconnect() 
--- HTML5/cms/server/main.py
+++ HTML5/cms/server/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 ###############################################################################
 #   
 #  Bonsai server
@@ -7,12 +10,22 @@
 ################################################################################
 from flask import Flask, jsonify, render_template
 import json
+import db
 
 from jsonp import support_jsonp
 
 # vars
 app = Flask(__name__)
 VERSION=0.1
+
+# DB
+def open_db():
+    """Creates the database tables."""
+    d = db()
+
+@app.teardown_appcontext
+def close_db(error):
+    db.disconnect();
 
 # API
 @app.route('/get/<app_id>/',  methods=['GET'])
