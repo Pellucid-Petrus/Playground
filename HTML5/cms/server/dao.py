9666b6806b9780fa64bb37d88ac369b5231030ab
Antonio Aloisio <gnuton@gnuton.org>
2015-02-21 09:13:14 +0200

    Fix PEP-8 warnings

--- HTML5/cms/server/dao.py
+++ HTML5/cms/server/dao.py
@@ -3,105 +3,116 @@ from db import db
 import hashlib
 import json
 
-class dao:
-  DEBUG = True
-  def __init__(self, config):
-    print "Instantiating DAO..."
-    self.db = db()
-    self.db.DEBUG = self.DEBUG
-    self.db.connect(config)
 
-  def __enter__(self):
-    return self
-  
-  def __exit__(self, type, value, traceback):
-    self.db.disconnect()
-
-  # Generic APIs
-  def commit(self):
-    self.db.commit();
-
-  # User APIs
-  def create_new_user(self, user, secret):
-    secret= self.__hash_secret(secret) 
-    q = 'INSERT INTO bonsai_app_config (user, secret, appname, config) VALUES ("%s", "%s", "", "{}");' % (user, secret)
-    res = self.db.query(q)
-  
-  def delete_user(self, user, secret):
-    if not user or not secret:
-      raise Exception("delete_user requires user or secret")
-
-    secret= self.__hash_secret(secret)
-    q = "delete from bonsai_app_config where user='%s' and secret='%s'" % (user, secret);
-    res = self.db.query(q)
-
-  # App config APIs
-  def get_app_config(self, user):
-    if user == None:
-      return None 
-    query = 'select config from bonsai_app_config where user="%s";' % user
-    res = self.db.query(query)
-    if res != (): 
-      return res[0][0] # (('{"foo":"bar"}',),)
-    else:
-      return {}
-
-  def update_user_data(self, user, config, secret, appname=None):
-    """ user, config, secret are mandatory arguments
-        NOTE: SECRET IS OPTIONAL FOR THE TIME BEING - NO SECURITY
+class Dao:
     """
-    if isinstance(config, dict):
-      config = json.dumps(config)
+    Interface to the DB
+    """
+    DEBUG = True
 
-    elif isinstance(config, str):
-      self.__validate_json(config)
+    def __init__(self, config):
+        self.db = db()
+        self.db.DEBUG = self.DEBUG
+        self.__dbg_msg("Initializing DAO...")
+        self.db.connect(config)
 
-    print type(config)
-    print config
+    def __enter__(self):
+        return self
 
-    if not user or not config:
-      print "NO USER or INVALID CONFIG"
-      return False
-    config = self.db.escape_str(config)
-    secret= self.__hash_secret(secret)
+    def __exit__(self, type, value, traceback):
+        self.db.disconnect()
 
-    q= 'UPDATE bonsai_app_config SET '
-    if secret != None:
-      q +='secret="%s",' % secret
-    if appname != None:
-      q +='appname="%s",' % appname
-      
-    q += 'config="%s" where user="%s"' % (config, user);
-    print q
-    res = self.db.query(q)
-    return True
 
-  def __hash_secret(self, secret):
-    print hashlib.sha256(secret).hexdigest()
+    # Common Private methods
+    def __dbg_msg(self, msg):
+        if self.DEBUG:
+            print msg
 
-  def __validate_json(self, json_str):
-    """ Throw ValueError exception if the string is not JSON """
-    json.loads(json_str)
+    def __hash_secret(self, secret):
+        hash = hashlib.sha256(secret).hexdigest()
+        return hash
+
+    def __validate_json(self, json_str):
+        """ Throw ValueError exception if the string is not JSON """
+        json.loads(json_str)
+
+
+    # Generic APIs
+    def commit(self):
+        self.db.commit();
+
+    # User APIs
+    def create_new_user(self, user, secret):
+        secret= self.__hash_secret(secret)
+        q = 'INSERT INTO bonsai_app_config (user, secret, appname, config) VALUES ("%s", "%s", "", "{}");' % (user, secret)
+        res = self.db.query(q)
+        return res
+
+    def delete_user(self, user, secret):
+        if not user or not secret:
+            raise Exception("delete_user requires user or secret")
+
+        secret= self.__hash_secret(secret)
+        q = "delete from bonsai_app_config where user='%s' and secret='%s'" % (user, secret);
+        res = self.db.query(q)
+
+    # App config APIs
+    def get_app_config(self, user):
+        if user == None:
+            return None
+        query = 'select config from bonsai_app_config where user="%s";' % user
+        res = self.db.query(query)
+        if res != ():
+            return res[0][0] # (('{"foo":"bar"}',),)
+        else:
+            return {}
+
+    def update_user_data(self, user, config, secret, appname=None):
+        """ user, config, secret are mandatory arguments
+            NOTE: SECRET IS OPTIONAL FOR THE TIME BEING - NO SECURITY
+        """
+        if isinstance(config, dict):
+            config = json.dumps(config)
+        elif isinstance(config, str):
+            self.__validate_json(config)
+
+        print type(config)
+        print config
+
+        if not user or not config:
+            print "NO USER or INVALID CONFIG"
+            return False
+        config = self.db.escape_str(config)
+        secret = self.__hash_secret(secret)
+
+        q = 'UPDATE bonsai_app_config SET '
+        if secret != None:
+            q += 'secret="%s",' % secret
+        if appname != None:
+            q += 'appname="%s",' % appname
+
+        q += 'config="%s" where user="%s"' % (config, user);
+        print q
+        res = self.db.query(q)
+        return True
 
 if __name__ == "__main__":
-  config = dict(
-    DB='testdb',
-    HOSTNAME='localhost',
-    USER='testuser',
-    PASSWORD='testpass') 
-  
-  with dao(config) as dd:
-    # insert okay
-    dd.create_new_user("fabio", "thepassword");
-    # fail to insert because same key is already in
-    dd.create_new_user("fabio", "thepassword");
-    
-    # set app config
-    dd.update_user_data("fabio", '{"foo":"bar"}', "thepassword")    
-    # get app config
-    print "APP CONFIG FOR FABIO:" + str(dd.get_app_config("fabio"));
+    config = dict(
+        DB='testdb',
+        HOSTNAME='localhost',
+        USER='testuser',
+        PASSWORD='testpass')
 
-    # delete user
-    #dd.delete_user("fabio");
+    with Dao(config) as dd:
+        # insert okay
+        dd.create_new_user("fabio", "thepassword");
+        # fail to insert because same key is already in
+        dd.create_new_user("fabio", "thepassword");
 
-     
+        # set app config
+        dd.update_user_data("fabio", '{"foo":"bar"}', "thepassword")
+        # get app config
+        print "APP CONFIG FOR FABIO:" + str(dd.get_app_config("fabio"));
+
+        # delete user
+        #dd.delete_user("fabio");
--- HTML5/cms/server/main.py
+++ HTML5/cms/server/main.py
@@ -10,13 +10,13 @@
 ################################################################################
 from flask import Flask, jsonify, render_template, request
 import json
-from dao import dao
+from dao import Dao
 import os
 from jsonp import support_jsonp
 
 # vars
 app = Flask(__name__)
-VERSION=0.1
+VERSION = 0.1
 
 # config
 app.config.update(dict(
@@ -31,7 +31,7 @@ app.config.update(dict(
 @app.route('/get/<user>/<app_id>/',  methods=['GET'])
 @support_jsonp
 def get_data(user, app_id):
-    #TODO user app_id
+    # TODO user app_id
     # show the user profile for that user
     app_config = dd.get_app_config(user)
     res = jsonify(json.loads(app_config))
@@ -53,11 +53,11 @@ def set_data(user, app_id):
 
 @app.route('/features')
 def features():
-  return render_template('features.html')
+    return render_template('features.html')
 
 @app.route('/dashboard')
 def dashboard():
-  return render_template('dashboard2.html', version=VERSION)
+    return render_template('dashboard2.html', version=VERSION)
 
 @app.route('/')
 def welcome():
@@ -66,5 +66,5 @@ def welcome():
 # main
 if __name__ == '__main__':
     app.debug = True
-    with dao(app.config) as dd:
-      app.run()
+    with Dao(app.config) as dd:
+        app.run()
